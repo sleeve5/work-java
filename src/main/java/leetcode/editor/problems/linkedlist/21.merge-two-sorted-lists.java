10ff4e5b7bc94dcbeaa6fcfea84a52ff57f1ca2f
sleeve5 <1244595156@qq.com>
2026-01-04 17:02:37 +0800

    initial

---
+++ src/main/java/leetcode/editor/problems/linkedlist/21.merge-two-sorted-lists.java
@@ -0,0 +1,53 @@
+package leetcode.editor.problems.linkedlist;
+
+import leetcode.editor.common.ListNode;
+
+/*
+ * @lc app=leetcode.cn id=21 lang=java
+ * @lcpr version=30305
+ *
+ * [21] 合并两个有序链表
+ */
+
+// @lc code=start
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        // 虚拟头结点
+        ListNode dummy = new ListNode(-1);
+        ListNode p = dummy;
+        ListNode p1 = list1;
+        ListNode p2 = list2;
+
+        while (p1 != null && p2 != null) {
+            // 比较 p1 和 p2 两个指针
+            // 将值较小的节点接到 p 指针
+            if (p1.val > p2.val) {
+                p.next = p2;
+                p2 = p2.next;
+            } else {
+                p.next = p1;
+                p1 = p1.next;
+            }
+            // p 指针不断前进
+            p = p.next;
+        }
+
+        if (p1 != null) {
+            p.next = p1;
+        }
+
+        if (p2 != null) {
+            p.next = p2;
+        }
+
+        return dummy.next;
+    }
+}
+// @lc code=end
+
+/*
+ * // @lcpr case=start
+ * // [1,2,4]\n[1,3,4]\n
+ * // @lcpr case=end
+ * 
+ */
