c2e5709788e52436af86e9c93a71dc8582f35cdc
sleeve5 <1244595156@qq.com>
2026-01-06 09:55:56 +0800

    initialize file structure

---
+++ src/main/java/leetcode/editor/problems/linkedlist/21.merge-two-sorted-lists.java
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=java
+ * @lcpr version=30305
+ *
+ * [21] 合并两个有序链表
+ */
+
+package leetcode.editor.problems.linkedlist;
+
+import leetcode.editor.common.ListNode;
+
+    // @lc code=start
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+            
+        }
+    }
+    // @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,4]\n[1,3,4]\n
+// @lcpr case=end
+
+ */
+
