e9d285e12b26b7f1c2b42a12a2addd08eaef5152
sleeve5 <1244595156@qq.com>
2026-01-08 15:44:29 +0800

    lists

--- src/main/java/practice/lists.java
+++ src/main/java/practice/lists.java
@@ -7,14 +7,15 @@ import java.util.List;
 
 public class lists {
     public static void main(String[] args) {
-        List<Integer> myList = new ArrayList<>();
+        // List<Integer> myList = new ArrayList<>();
         Integer[] myArray = new Integer[] { 1, 2, 3, 4, 5 };
         List<Integer> nums = new ArrayList<>(Arrays.asList(myArray));
 
-        int num = nums.get(2);
+        // int num = nums.get(2);
         nums.set(3, 5);
         nums.add(3);
         nums.add(4, 6);
+        nums.remove(3);
         for (int i = 0; i < nums.size(); i++) {
             System.out.println(nums.get(i));
         }
