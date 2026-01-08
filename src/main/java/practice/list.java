391738c2d81add8fd84d8c8e7b43c52b6a45ad78
sleeve5 <1244595156@qq.com>
2026-01-08 17:13:17 +0800

    list

--- src/main/java/practice/list.java
+++ src/main/java/practice/list.java
@@ -87,26 +87,26 @@ public class list {
     }
 
     public static void main(String[] args) {
-        // // List<Integer> myList = new ArrayList<>();
-        // Integer[] myArray = new Integer[] { 1, 2, 3, 4, 5 };
-        // List<Integer> nums = new ArrayList<>(Arrays.asList(myArray));
+        // List<Integer> myList = new ArrayList<>();
+        Integer[] myArray = new Integer[] { 1, 2, 3, 4, 5 };
+        List<Integer> nums = new ArrayList<>(Arrays.asList(myArray));
 
-        // // int num = nums.get(2);
-        // nums.set(3, 5);
-        // nums.add(3);
-        // nums.add(4, 6);
-        // nums.remove(3);
-        // for (int i = 0; i < nums.size(); i++) {
-        // System.out.println(nums.get(i));
-        // }
-        // System.out.println('\n');
-        // List<Integer> myList2 = new ArrayList<>(Arrays.asList(new Integer[] { 3, 5, 7
-        // }));
-        // nums.addAll(myList2);
-        // Collections.sort(nums);
-        // for (int i = 0; i < nums.size(); i++) {
-        // System.out.println(nums.get(i));
-        // }
+        // int num = nums.get(2);
+        nums.set(3, 5);
+        nums.add(3);
+        nums.add(4, 6);
+        nums.remove(3);
+        for (int i = 0; i < nums.size(); i++) {
+            System.out.println(nums.get(i));
+        }
+        System.out.println('\n');
+        List<Integer> myList2 = new ArrayList<>(Arrays.asList(new Integer[] { 3, 5, 7
+        }));
+        nums.addAll(myList2);
+        Collections.sort(nums);
+        for (int i = 0; i < nums.size(); i++) {
+            System.out.println(nums.get(i));
+        }
 
         // 创建测试实例
         list outer = new list();
