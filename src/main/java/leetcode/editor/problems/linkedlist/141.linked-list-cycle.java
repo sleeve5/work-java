5ca3ad5ea3738aec3febbab4dee8b4681306e377
sleeve5 <1244595156@qq.com>
2026-01-22 15:16:39 +0800

    [141] 环形链表

---
+++ src/main/java/leetcode/editor/problems/linkedlist/141.linked-list-cycle.java
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode.cn id=141 lang=java
+ * @lcpr version=30307
+ *
+ * [141] 环形链表
+ */
+
+package leetcode.editor.problems.linkedlist;
+
+import leetcode.editor.common.ListNode;
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) {
+ * val = x;
+ * next = null;
+ * }
+ * }
+ */
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+// @lc code=end
+
+/*
+ * // @lcpr case=start
+ * // [3,2,0,-4]\n1\n
+ * // @lcpr case=end
+ * 
+ * // @lcpr case=start
+ * // [1,2]\n0\n
+ * // @lcpr case=end
+ * 
+ * // @lcpr case=start
+ * // [1]\n-1\n
+ * // @lcpr case=end
+ * 
+ */
