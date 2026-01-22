a234242674c01624cd763f7f52aa2eda1cf2f444
sleeve5 <1244595156@qq.com>
2026-01-22 15:46:24 +0800

    [876] 链表的中间结点

---
+++ src/main/java/leetcode/editor/problems/linkedlist/876.middle-of-the-linked-list.java
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode.cn id=876 lang=java
+ * @lcpr version=30307
+ *
+ * [876] 链表的中间结点
+ */
+
+package leetcode.editor.problems.linkedlist;
+
+import leetcode.editor.common.ListNode;
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
+// @lc code=end
+
+/*
+ * // @lcpr case=start
+ * // [1,2,3,4,5]\n
+ * // @lcpr case=end
+ * 
+ * // @lcpr case=start
+ * // [1,2,3,4,5,6]\n
+ * // @lcpr case=end
+ * 
+ */
