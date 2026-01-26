ac6fae9fe1242b19ffb238f130dceb40801be66c
sleeve5 <1244595156@qq.com>
2026-01-26 16:02:57 +0800

    [206] 反转链表

---
+++ src/main/java/leetcode/editor/problems/linkedlist/206.reverse-linked-list.java
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=java
+ * @lcpr version=30307
+ *
+ * [206] 反转链表
+ */
+
+package leetcode.editor.problems.linkedlist;
+
+import leetcode.editor.common.ListNode;
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode last = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return last;
+    }
+}
+// @lc code=end
+
+/*
+ * // @lcpr case=start
+ * // [1,2,3,4,5]\n
+ * // @lcpr case=end
+ * 
+ * // @lcpr case=start
+ * // [1,2]\n
+ * // @lcpr case=end
+ * 
+ * // @lcpr case=start
+ * // []\n
+ * // @lcpr case=end
+ * 
+ */
