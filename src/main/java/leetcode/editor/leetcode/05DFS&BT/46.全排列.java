c802cb9c930cee51f5880df694a8be8b8d207555
sleeve5 <1244595156@qq.com>
2026-02-26 22:47:17 +0800

    [46] 全排列

---
+++ src/main/java/leetcode/editor/leetcode/05DFS&BT/46.全排列.java
@@ -0,0 +1,106 @@
+/*
+ * @lc app=leetcode.cn id=46 lang=java
+ * @lcpr version=30400
+ *
+ * [46] 全排列
+ *
+ * https://leetcode.cn/problems/permutations/description/
+ *
+ * algorithms
+ * Medium (80.46%)
+ * Likes:    3256
+ * Dislikes: 0
+ * Total Accepted:    1.6M
+ * Total Submissions: 2M
+ * Testcase Example:  '[1,2,3]\n[0,1]\n[1]'
+ *
+ * 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：nums = [1,2,3]
+ * 输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入：nums = [0,1]
+ * 输出：[[0,1],[1,0]]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 输入：nums = [1]
+ * 输出：[[1]]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= nums.length <= 6
+ * -10 <= nums[i] <= 10
+ * nums 中的所有整数 互不相同
+ * 
+ * 
+ */
+
+// @lc code=start
+
+import java.util.LinkedList;
+import java.util.List;
+
+class Solution {
+    List<List<Integer>> res = new LinkedList<>();
+    
+    public List<List<Integer>> permute(int[] nums) {
+        int n = nums.length;
+        LinkedList<Integer> track = new LinkedList<>();
+        boolean[] used = new boolean[n];
+
+        backtrack(nums, track, used);
+
+        return res;
+    }
+
+    private void backtrack(int[] nums, LinkedList<Integer> track, boolean[] used) {
+        if (track.size() == nums.length) {
+            res.add(new LinkedList(track));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) {
+                continue;
+            }
+
+            track.add(nums[i]);
+            used[i] = true;
+            backtrack(nums, track, used);
+            track.removeLast();
+            used[i] = false;
+        }
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [0,1]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n
+// @lcpr case=end
+
+ */
+
