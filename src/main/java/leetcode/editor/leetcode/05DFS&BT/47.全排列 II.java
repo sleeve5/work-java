4159af54507bf27fad445e9804a76d587e46dc21
sleeve5 <1244595156@qq.com>
2026-02-27 00:04:26 +0800

    [47] 全排列 II

---
+++ src/main/java/leetcode/editor/leetcode/05DFS&BT/47.全排列 II.java
@@ -0,0 +1,104 @@
+/*
+ * @lc app=leetcode.cn id=47 lang=java
+ * @lcpr version=30400
+ *
+ * [47] 全排列 II
+ *
+ * https://leetcode.cn/problems/permutations-ii/description/
+ *
+ * algorithms
+ * Medium (66.84%)
+ * Likes:    1773
+ * Dislikes: 0
+ * Total Accepted:    715K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[1,1,2]\n[1,2,3]'
+ *
+ * 给定一个可包含重复数字的序列 nums ，按任意顺序 返回所有不重复的全排列。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：nums = [1,1,2]
+ * 输出：
+ * [[1,1,2],
+ * ⁠[1,2,1],
+ * ⁠[2,1,1]]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入：nums = [1,2,3]
+ * 输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= nums.length <= 8
+ * -10 <= nums[i] <= 10
+ * 
+ * 
+ */
+
+// @lc code=start
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+class Solution {
+    private List<List<Integer>> res = new LinkedList<>();
+    private LinkedList<Integer> track = new LinkedList<>();
+    boolean[] used;
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
+        used = new boolean[nums.length];
+
+        backtrack(nums);
+
+        return res;
+    }
+
+    private void backtrack(int[] nums) {
+        if (track.size() == nums.length) {
+            res.add(new LinkedList<>(track));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) {
+                continue;
+            }
+            
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) {
+                continue;
+            }
+
+            track.addLast(nums[i]);
+            used[i] = true;
+            backtrack(nums);
+            track.removeLast();
+            used[i] = false;
+        }
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,1,2]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2,3]\n
+// @lcpr case=end
+
+ */
+
