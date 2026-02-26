d8b0917ba1579c8e50280a346733d8b9f646dc5b
sleeve5 <1244595156@qq.com>
2026-02-26 15:55:42 +0800

    [516] 最长回文子序列

---
+++ src/main/java/leetcode/editor/leetcode/08DP-advanced/516.最长回文子序列.java
@@ -0,0 +1,90 @@
+/*
+ * @lc app=leetcode.cn id=516 lang=java
+ * @lcpr version=30400
+ *
+ * [516] 最长回文子序列
+ *
+ * https://leetcode.cn/problems/longest-palindromic-subsequence/description/
+ *
+ * algorithms
+ * Medium (67.65%)
+ * Likes:    1402
+ * Dislikes: 0
+ * Total Accepted:    339.4K
+ * Total Submissions: 501.7K
+ * Testcase Example:  '"bbbab"\n"cbbd"'
+ *
+ * 给你一个字符串 s ，找出其中最长的回文子序列，并返回该序列的长度。
+ * 
+ * 子序列定义为：不改变剩余字符顺序的情况下，删除某些字符或者不删除任何字符形成的一个序列。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：s = "bbbab"
+ * 输出：4
+ * 解释：一个可能的最长回文子序列为 "bbbb" 。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入：s = "cbbd"
+ * 输出：2
+ * 解释：一个可能的最长回文子序列为 "bb" 。
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= s.length <= 1000
+ * s 仅由小写英文字母组成
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        int n = s.length();
+
+        if (n == 1) {
+            return 1;
+        }
+
+        int[][] dp = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 1;
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[0][n - 1];
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// "bbbab"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "cbbd"\n
+// @lcpr case=end
+
+ */
+
