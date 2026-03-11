0d20032da87800ce034be59f9f02b96c0f16e607
sleeve5 <1244595156@qq.com>
2026-03-11 21:32:35 +0800

    [98] 验证二叉搜索树

---
+++ 98.validate-binary-search-tree.java
@@ -0,0 +1,106 @@
+/*
+ * @lc app=leetcode.cn id=98 lang=java
+ * @lcpr version=30400
+ *
+ * [98] 验证二叉搜索树
+ *
+ * https://leetcode.cn/problems/validate-binary-search-tree/description/
+ *
+ * algorithms
+ * Medium (40.60%)
+ * Likes:    2726
+ * Dislikes: 0
+ * Total Accepted:    1.4M
+ * Total Submissions: 3.4M
+ * Testcase Example:  '[2,1,3]\n[5,1,4,null,null,3,6]'
+ *
+ * 给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。
+ * 
+ * 有效 二叉搜索树定义如下：
+ * 
+ * 
+ * 节点的左子树只包含 严格小于 当前节点的数。
+ * 节点的右子树只包含 严格大于 当前节点的数。
+ * 所有左子树和右子树自身必须也是二叉搜索树。
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：root = [2,1,3]
+ * 输出：true
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入：root = [5,1,4,null,null,3,6]
+ * 输出：false
+ * 解释：根节点的值是 5 ，但是右子节点的值是 4 。
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中节点数目范围在[1, 10^4] 内
+ * -2^31 <= Node.val <= 2^31 - 1
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, null, null);
+    }
+
+    boolean isValidBST(TreeNode root, TreeNode min, TreeNode max) {
+        if (root == null) {
+            return true;
+        }
+
+        if (min != null && root.val <= min.val) {
+            return false;
+        }
+
+        if (max != null && root.val >= max.val) {
+            return false;
+        }
+
+        return isValidBST(root.left, min, root) && isValidBST(root.right, root, max);
+    }
+
+    class TreeNode {
+        TreeNode left, right;
+        int val;
+    }
+}
+// @lc code=end
+
+/*
+ * // @lcpr case=start
+ * // [2,1,3]\n
+ * // @lcpr case=end
+ * 
+ * // @lcpr case=start
+ * // [5,1,4,null,null,3,6]\n
+ * // @lcpr case=end
+ * 
+ */
