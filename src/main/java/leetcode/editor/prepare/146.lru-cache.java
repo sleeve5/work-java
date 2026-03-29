82d2366d02000df7e4c040f069da5150942d30aa
sleeve5 <1244595156@qq.com>
2026-03-29 20:58:17 +0800

    prepare

---
+++ src/main/java/leetcode/editor/prepare/146.lru-cache.java
@@ -0,0 +1,239 @@
+/*
+ * @lc app=leetcode.cn id=146 lang=java
+ * @lcpr version=30400
+ *
+ * [146] LRU 缓存
+ *
+ * https://leetcode.cn/problems/lru-cache/description/
+ *
+ * algorithms
+ * Medium (55.29%)
+ * Likes:    3732
+ * Dislikes: 0
+ * Total Accepted:    1M
+ * Total Submissions: 1.9M
+ * Testcase Example:  '["LRUCache","put","put","get","put","get","put","get","get","get"]\n' +
+  '[[2],[1,1],[2,2],[1],[3,3],[2],[4,4],[1],[3],[4]]'
+ *
+ * 请你设计并实现一个满足  LRU (最近最少使用) 缓存 约束的数据结构。
+ * 
+ * 实现 LRUCache 类：
+ * 
+ * 
+ * 
+ * 
+ * LRUCache(int capacity) 以 正整数 作为容量 capacity 初始化 LRU 缓存
+ * int get(int key) 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1 。
+ * void put(int key, int value) 如果关键字 key 已经存在，则变更其数据值 value ；如果不存在，则向缓存中插入该组
+ * key-value 。如果插入操作导致关键字数量超过 capacity ，则应该 逐出 最久未使用的关键字。
+ * 
+ * 
+ * 函数 get 和 put 必须以 O(1) 的平均时间复杂度运行。
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 示例：
+ * 
+ * 输入
+ * ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+ * [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+ * 输出
+ * [null, null, null, 1, null, -1, null, -1, 3, 4]
+ * 
+ * 解释
+ * LRUCache lRUCache = new LRUCache(2);
+ * lRUCache.put(1, 1); // 缓存是 {1=1}
+ * lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
+ * lRUCache.get(1);    // 返回 1
+ * lRUCache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+ * lRUCache.get(2);    // 返回 -1 (未找到)
+ * lRUCache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
+ * lRUCache.get(1);    // 返回 -1 (未找到)
+ * lRUCache.get(3);    // 返回 3
+ * lRUCache.get(4);    // 返回 4
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= capacity <= 3000
+ * 0 <= key <= 10000
+ * 0 <= value <= 10^5
+ * 最多调用 2 * 10^5 次 get 和 put
+ * 
+ * 
+ */
+
+// @lc code=start
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+class Node {
+    public int key, val;
+    public Node next, prev;
+
+    public Node(int k, int v) {
+        this.key = k;
+        this.val = v;
+    }
+}
+
+class DoubleList {
+    private Node head, tail;
+    private int size;
+
+    public DoubleList() {
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+        head.next = tail;
+        tail.prev = head;
+        size = 0;
+    }
+
+    public void addLast(Node x) {
+        x.prev = tail.prev;
+        x.next = tail;
+        tail.prev.next = x;
+        tail.prev = x;
+        size++;
+    }
+
+    public void remove(Node x) {
+        x.prev.next = x.next;
+        x.next.prev = x.prev;
+        size--;
+    }
+
+    public Node removeFirst() {
+        if (head.next == tail) {
+            return null;
+        }
+        Node first = head.next;
+        remove(first);
+        return first;
+    }
+
+    public int size() {
+        return size;
+    }
+}
+
+class LRUCache {
+    private HashMap<Integer, Node> map;
+    private DoubleList cache;
+    private int cap;
+
+    public LRUCache(int capacity) {
+        this.cap = capacity;
+        map = new HashMap<>();
+        cache = new DoubleList();
+    }
+
+    private void makeRecently(int key) {
+        Node x = map.get(key);
+        cache.remove(x);
+        cache.addLast(x);
+    }
+
+    private void addRecently(int key, int val) {
+        Node x = new Node(key, val);
+        cache.addLast(x);
+        map.put(key, x);
+    }
+
+    private void deleteKey(int key) {
+        Node x = map.get(key);
+        cache.remove(x);
+        map.remove(key);
+    }
+
+    private void removeLeastRecently() {
+        Node x = cache.removeFirst();
+        int k = x.key;
+        map.remove(k);
+    }
+
+    public int get(int key) {
+        if (!map.containsKey(key)) {
+            return -1;
+        }
+
+        makeRecently(key);
+        return map.get(key).val;
+    }
+
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            deleteKey(key);
+            addRecently(key, value);
+            return;
+        }
+
+        if (cap == cache.size()) {
+            removeLeastRecently();
+        }
+
+        addRecently(key, value);
+    }
+}
+
+// class LRUCache {
+// private int cap;
+// private LinkedHashMap<Integer, Integer> cache = new LinkedHashMap<>();
+
+// public LRUCache(int capacity) {
+// this.cap = capacity;
+// }
+
+// public int get(int key) {
+// if (!cache.containsKey(key)) {
+// return -1;
+// }
+
+// makeRecently(key);
+
+// return cache.get(key);
+// }
+
+// public void put(int key, int val) {
+// if (cache.containsKey(key)) {
+// cache.put(key, val);
+// makeRecently(key);
+// return;
+// }
+
+// if (cache.size() >= this.cap) {
+// int oldKey = cache.keySet().iterator().next();
+// cache.remove(oldKey);
+// }
+
+// cache.put(key, val);
+// }
+
+// private void makeRecently(int key) {
+// int val = cache.get(key);
+// cache.remove(key);
+// cache.put(key, val);
+// }
+// }
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
+// @lc code=end
+
+/*
+ * // @lcpr case=start
+ * //
+ * ["LRUCache","put","put","get","put","get","put","get","get","get"]\n[[2],[1,1
+ * ],[2,2],[1],[3,3],[2],[4,4],[1],[3],[4]]\n
+ * // @lcpr case=end
+ * 
+ */
---
+++ src/main/java/leetcode/editor/prepare/200.岛屿数量.java
@@ -0,0 +1,113 @@
+/*
+ * @lc app=leetcode.cn id=200 lang=java
+ * @lcpr version=30400
+ *
+ * [200] 岛屿数量
+ *
+ * https://leetcode.cn/problems/number-of-islands/description/
+ *
+ * algorithms
+ * Medium (63.75%)
+ * Likes:    2933
+ * Dislikes: 0
+ * Total Accepted:    1.3M
+ * Total Submissions: 2M
+ * Testcase Example:  '[["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]\n' +
+  '[["1","1","0","0","0"],["1","1","0","0","0"],["0","0","1","0","0"],["0","0","0","1","1"]]'
+ *
+ * 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+ * 
+ * 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+ * 
+ * 此外，你可以假设该网格的四条边均被水包围。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：grid = [
+ * ['1','1','1','1','0'],
+ * ['1','1','0','1','0'],
+ * ['1','1','0','0','0'],
+ * ['0','0','0','0','0']
+ * ]
+ * 输出：1
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入：grid = [
+ * ['1','1','0','0','0'],
+ * ['1','1','0','0','0'],
+ * ['0','0','1','0','0'],
+ * ['0','0','0','1','1']
+ * ]
+ * 输出：3
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * m == grid.length
+ * n == grid[i].length
+ * 1 <= m, n <= 300
+ * grid[i][j] 的值为 '0' 或 '1'
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    private int[][] directions = new int[][] {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    private int res = 0;
+
+    public int numIslands(char[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    res++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+
+        return res;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        int m = grid.length, n = grid[0].length;
+        if (i < 0 || i >= m || j < 0 || j >= n) {
+            return;
+        }
+
+        if (grid[i][j] == '0') {
+            return;
+        }
+
+        grid[i][j] = '0';
+
+        for (int[] dir : directions) {
+            int next_i = i + dir[0];
+            int next_j = j + dir[1];
+            dfs(grid, next_i, next_j);
+        }
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [["1","1","0","0","0"],["1","1","0","0","0"],["0","0","1","0","0"],["0","0","0","1","1"]]\n
+// @lcpr case=end
+
+ */
+
