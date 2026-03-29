ac4cc3ea1db69f03734ebd93e358e314ca92adae
sleeve5 <1244595156@qq.com>
2026-03-29 20:53:58 +0800

    [207] 课程表

---
+++ src/main/java/leetcode/editor/leetcode/09graph/207.course-schedule.java
@@ -0,0 +1,120 @@
+/*
+ * @lc app=leetcode.cn id=207 lang=java
+ * @lcpr version=30401
+ *
+ * [207] 课程表
+ *
+ * https://leetcode.cn/problems/course-schedule/description/
+ *
+ * algorithms
+ * Medium (56.67%)
+ * Likes:    2339
+ * Dislikes: 0
+ * Total Accepted:    731K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '2\n[[1,0]]\n2\n[[1,0],[0,1]]'
+ *
+ * 你这个学期必须选修 numCourses 门课程，记为 0 到 numCourses - 1 。
+ * 
+ * 在选修某些课程之前需要一些先修课程。 先修课程按数组 prerequisites 给出，其中 prerequisites[i] = [ai, bi]
+ * ，表示如果要学习课程 ai 则 必须 先学习课程  bi 。
+ * 
+ * 
+ * 例如，先修课程对 [0, 1] 表示：想要学习课程 0 ，你需要先完成课程 1 。
+ * 
+ * 
+ * 请你判断是否可能完成所有课程的学习？如果可以，返回 true ；否则，返回 false 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：numCourses = 2, prerequisites = [[1,0]]
+ * 输出：true
+ * 解释：总共有 2 门课程。学习课程 1 之前，你需要完成课程 0 。这是可能的。
+ * 
+ * 示例 2：
+ * 
+ * 输入：numCourses = 2, prerequisites = [[1,0],[0,1]]
+ * 输出：false
+ * 解释：总共有 2 门课程。学习课程 1 之前，你需要先完成​课程 0 ；并且学习课程 0 之前，你还应先完成课程 1 。这是不可能的。
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= numCourses <= 2000
+ * 0 <= prerequisites.length <= 5000
+ * prerequisites[i].length == 2
+ * 0 <= ai, bi < numCourses
+ * prerequisites[i] 中的所有课程对 互不相同
+ * 
+ * 
+ */
+
+// @lc code=start
+
+import java.util.*;
+
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<Integer>[] graph = buildGraph(numCourses, prerequisites);
+        int[] indegree = new int[numCourses];
+
+        for (int[] edge : prerequisites) {
+            int to = edge[0];
+            indegree[to]++;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.offer(i);
+            }
+        }
+
+        int count = 0;
+
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            count++;
+            for (int next : graph[cur]) {
+                indegree[next]--;
+                if (indegree[next] == 0) {
+                    q.offer(next);
+                }
+            }
+        }
+
+        return count == numCourses;
+    }
+
+    private List<Integer>[] buildGraph(int numCourses, int[][] prerequisites) {
+        List<Integer>[] graph = new LinkedList[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+            graph[i] = new LinkedList<>();
+        }
+
+        for (int[] edge : prerequisites) {
+            int from = edge[1], to = edge[0];
+            graph[from].add(to);
+        }
+
+        return graph;
+    }
+}
+// @lc code=end
+
+/*
+ * // @lcpr case=start
+ * // 2\n[[1,0]]\n
+ * // @lcpr case=end
+ * 
+ * // @lcpr case=start
+ * // 2\n[[1,0],[0,1]]\n
+ * // @lcpr case=end
+ * 
+ */
