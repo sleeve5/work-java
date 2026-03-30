0d141806631cdd42dd5d4b97ebb37b97b85ba35e
sleeve5 <1244595156@qq.com>
2026-03-30 21:20:39 +0800

    [34] 在排序数组中查找元素的第一个和最后一个位置

---
+++ src/main/java/leetcode/editor/leetcode/06binarysearch/34.find-first-and-last-position-of-element-in-sorted-array.java
@@ -0,0 +1,102 @@
+/*
+ * @lc app=leetcode.cn id=34 lang=java
+ * @lcpr version=30401
+ *
+ * [34] 在排序数组中查找元素的第一个和最后一个位置
+ *
+ * https://leetcode.cn/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+ *
+ * algorithms
+ * Medium (46.38%)
+ * Likes:    3236
+ * Dislikes: 0
+ * Total Accepted:    1.5M
+ * Total Submissions: 3.2M
+ * Testcase Example:  '[5,7,7,8,8,10]\n8\n[5,7,7,8,8,10]\n6\n[]\n0'
+ *
+ * 给你一个按照非递减顺序排列的整数数组 nums，和一个目标值 target。请你找出给定目标值在数组中的开始位置和结束位置。
+ * 
+ * 如果数组中不存在目标值 target，返回 [-1, -1]。
+ * 
+ * 你必须设计并实现时间复杂度为 O(log n) 的算法解决此问题。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：nums = [5,7,7,8,8,10], target = 8
+ * 输出：[3,4]
+ * 
+ * 示例 2：
+ * 
+ * 输入：nums = [5,7,7,8,8,10], target = 6
+ * 输出：[-1,-1]
+ * 
+ * 示例 3：
+ * 
+ * 输入：nums = [], target = 0
+ * 输出：[-1,-1]
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 0 <= nums.length <= 10^5
+ * -10^9 <= nums[i] <= 10^9
+ * nums 是一个非递减数组
+ * -10^9 <= target <= 10^9
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        if (nums.length == 0) {
+            return new int[] { -1, -1 };
+        }
+
+        return new int[] { findBound(nums, target, true), findBound(nums, target, false) };
+    }
+
+    private int findBound(int[] nums, int target, boolean isLeft) {
+        int left = 0, right = nums.length - 1;
+        int res = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                res = mid;
+                if (isLeft) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return res;
+    }
+}
+// @lc code=end
+
+/*
+ * // @lcpr case=start
+ * // [5,7,7,8,8,10]\n8\n
+ * // @lcpr case=end
+ * 
+ * // @lcpr case=start
+ * // [5,7,7,8,8,10]\n6\n
+ * // @lcpr case=end
+ * 
+ * // @lcpr case=start
+ * // []\n0\n
+ * // @lcpr case=end
+ * 
+ */
